900d2d143d7e803853a3df9f553eaac9ab541134
PallaviGupta21 <guptapallavi200@gmail.com>
2019-02-24 21:35:47 +0530

    my code uploaded...

---
+++ Git_calci/src/Calcu.java
@@ -0,0 +1,31 @@
+
+public class Calcu {
+
+	int x,y,add;
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getAdd() {
+		return add;
+	}
+
+	public void setAdd(int add) {
+		this.add = add;
+	}
+	
+	
+}
---
+++ Git_calci/src/Test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("enter the no. on which action is to be performed: ");
+		Scanner sc=new Scanner("System.in");
+		int x =sc.nextInt();
+		int y= sc.nextInt();
+        int sum = add(x,y);
+	}
+
+	public static int add(int x,int y){
+		return(x+y);
+	}
+}
