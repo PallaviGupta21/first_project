f5ef6d908e4eebc1015bef685a9f92c46abe9130
Pallavi Gupta <46018368+PallaviGupta21@users.noreply.github.com>
2019-07-29 10:09:17 +0530

    features added.

--- Git_calci/src/Test.java
+++ Git_calci/src/Test.java
@@ -14,4 +14,16 @@ public class Test {
 	public static int add(int x,int y){
 		return(x+y);
 	}
+	
+	public static int sub(int x,int y){
+		return(x-y);
+	}
+	
+	public static int mul(int x,int y){
+		return(x*y);
+	}
+	
+	public static int div(int x,int y){
+		return(x/y);
+	}
 }
